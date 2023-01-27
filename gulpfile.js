f7b7294cad0944be2d4ef683469a4f62f2b98d38
alexeyShuncko <shuncko.a@yandex.ru>
2023-01-27 14:36:23 +0300

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import csso from 'gulp-csso';
+import include from 'gulp-file-include';
+import htmlmin from 'gulp-htmlmin';
+import concat from 'gulp-concat';
+import autoprefixer from 'gulp-autoprefixer';
+import { deleteAsync } from 'del';
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+
+const { src, dest, series, watch } = gulp;
+const sass = gulpSass(dartSass);
+
+function htmlTask() {
+  return src('src/**.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('dist'));
+}
+
+function jsTask() {
+  return src('src/**.js').pipe(dest('dist'));
+}
+
+function scss() {
+  return src('src/scss/**.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(dest('dist'));
+}
+
+function serve() {
+  browserSync.init({
+    server: './dist',
+  });
+  watch('src/**/*.*', series(clear, scss, jsTask, htmlTask)).on(
+    'change',
+    browserSync.reload
+  );
+}
+
+function clear() {
+  return deleteAsync('dist');
+}
+gulp.task('default', series(clear, scss, jsTask, htmlTask, serve));
