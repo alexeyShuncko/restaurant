41f29e0fb52f6709617135d8802fd0e7d1a21d94
alexeyShuncko <shuncko.a@yandex.ru>
2023-01-28 16:06:58 +0300

    форма

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,3 +15,43 @@ menu.addEventListener('click', () => {
     list.classList.add('active');
   }
 });
+
+let btn = document.getElementById('btnForm');
+let body = document.querySelector('body');
+let mask = document.querySelector('.mask');
+let formContainer = document.querySelector('.formContainer');
+
+btn.addEventListener('click', (e) => {
+  mask.classList.add('shadow');
+  body.classList.add('shadow');
+  formContainer.classList.add('active');
+});
+
+let btnReserve = document.getElementById('btnReserve');
+
+btnReserve.addEventListener('click', (e) => {
+  e.preventDefault();
+  let formReserve = document.querySelector('.formReserve');
+  let data = {
+    name: formReserve.name.value,
+    email: formReserve.email.value,
+  };
+  mask.classList.remove('shadow');
+  body.classList.remove('shadow');
+  formContainer.classList.remove('active');
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((json) => alert('Table reserved!'));
+});
+
+let btnExit = document.getElementById('btnExit');
+
+btnExit.addEventListener('click', (e) => {
+  e.preventDefault();
+  mask.classList.remove('shadow');
+  body.classList.remove('shadow');
+  formContainer.classList.remove('active');
+});
